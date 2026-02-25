16aee4da3a1e5dcf58ced9a5065be98ff6d5da21
compainn <compainn@outlook.com>
2026-02-25 19:37:01 +0300

    fix express wildcard

--- backend/server.js
+++ backend/server.js
@@ -12,18 +12,17 @@ app.use(cors());
 app.use(express.json());
 app.use('/api', apiRoutes);
 
-// Создаём HTTP-сервер вручную (нужно для WebSocket)
+// fallback для React
+app.get("/*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../build/index.html"));
+});
+
 const server = http.createServer(app);
 
-// Подключаем WebSocket ракеты
 initRocketWS(server);
 
 const PORT = process.env.PORT || 3001;
 
 server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
-});
-
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../build/index.html"));
 });
